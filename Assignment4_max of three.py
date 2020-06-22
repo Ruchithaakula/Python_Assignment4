53938ced76617208cc08432ca940612de3c4a4ab
Ruchithaakula <67132687+Ruchithaakula@users.noreply.github.com>
2020-06-22 20:10:59 +0530

    Add files via upload

---
+++ Assignment4_max of three.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+def maximum(a, b, c): 
+    list = [a, b, c] 
+    return max(list) 
+   
+a = 10
+b = 19
+c = 64
+print(maximum(a, b, c)) 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
